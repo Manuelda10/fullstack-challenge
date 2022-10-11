1c83b4de3c9a31f0433641fdae7bfaef2f7364b2
Manuel Vallejos <manuel.vallejos@unmsm.edu.pe>
2022-10-11 03:15:48 -0500

    Add database models

---
+++ backend/src/models/association.js
@@ -0,0 +1,18 @@
+const Category = require('./category')
+const Transaction = require('./transaction')
+const Type = require('./type')
+const User = require('./user')
+
+Type.hasMany(Category, { onDelete: 'CASCADE' })
+Category.belongsTo(Type, { onDelete: 'CASCADE' })
+
+Type.hasMany(Transaction, {onDelete: 'CASCADE'})
+Transaction.belongsTo(Type, {onDelete: 'CASCADE'})
+
+Category.hasMany(Transaction, {onDelete: 'CASCADE'})
+Transaction.belongsTo(Category, { onDelete: 'CASCADE' })
+
+User.hasMany(Transaction, { onDelete: 'CASCADE' })
+Transaction.belongsTo(User, {onDelete: 'CASCADE'})
+
+module.exports = { Category, Transaction, Type, User }
---
+++ backend/src/models/user.js
@@ -0,0 +1,22 @@
+const { sequelize, Sequelize } = require('../database/db')
+
+const User = sequelize.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: false
+})
+
+module.exports = User
